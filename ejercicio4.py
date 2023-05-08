332826e75a99ed982b85c3c90139fef7bc8b1e89
galmarcano <genesismarc@gmail.com>
2023-05-08 18:21:55 -0400

    ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+horas = float(input("¿Cuántas hora trabajó? "))
+coste = float(input("Indique el valor de la hora trabajada: "))
+paga = horas * coste
+
+print(f"Le corresponde un pago total de {paga}$")
